81839dd481f13c3fac88c2916cec15bfe3e77024
Mdelacruzmelo <mdelacruzmelo@gmail.com>
2022-08-22 22:56:06 +0200

    TASK-1_Connected with cloud mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { ClientsModule } from './clients/clients.module';
 
 @Module({
-  imports: [ClientsModule],
+  imports: [
+    ClientsModule,
+    MongooseModule.forRoot('mongodb+srv://mario:mario123456@ufounders-db.d72zqjx.mongodb.net/?retryWrites=true&w=majority')
+  ],
+
 })
 export class AppModule { }
