f6487dd188aecf1ad8f73b69b541ea6af1bcc338
Mdelacruzmelo <mdelacruzmelo@gmail.com>
2022-08-25 00:16:27 +0200

    TASK-2 Ports and adapters http implemented

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { ClientsModule } from 'src/clients/clients.module';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
-  imports: [ClientsModule]
+  imports: [ClientsModule, CommonModule]
 })
 export class SeedModule { }
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,36 +1,35 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import axios, { AxiosInstance } from 'axios';
 import { Client } from 'src/clients/entities/client.entity';
 import { Client as ClientInterface } from './interfaces/client-response.interface';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
 
   constructor(
     @InjectModel(Client.name)
-    private readonly clientModel: Model<Client>
+    private readonly clientModel: Model<Client>,
+    private readonly http: AxiosAdapter,
   ) { }
-
-  private readonly axios: AxiosInstance = axios
 
   async seedDatabase() {
 
     // Fetch data
-    const { data } = await this.axios.get<ClientInterface[]>(
+    const data = await this.http.get<ClientInterface[]>(
       'https://6f7smj4fdc.execute-api.us-east-1.amazonaws.com/default/techJobMission'
     )
     if (!data)
       throw new InternalServerErrorException(
-        `Could not fill the database. Please contact support`
+        `Could not recover data to fill the database. Please contact support`
       )
 
     // Clean table after i get the data
-    this.clientModel.deleteMany()
+    await this.clientModel.deleteMany()
 
     // Insert multiple clients
-    this.clientModel.insertMany(data)
+    await this.clientModel.insertMany(data)
 
     return `Database reinitialized successfully`
 
