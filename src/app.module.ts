7cb382015e2be07978823960b82fa58f1fd05a54
Mdelacruzmelo <mdelacruzmelo@gmail.com>
2022-08-25 21:40:27 +0200

    Env variables added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { ConfigModule } from '@nestjs/config'
 import { ClientsModule } from './clients/clients.module';
 import { SeedModule } from './seed/seed.module';
 import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ClientsModule,
-    MongooseModule.forRoot('mongodb+srv://mario:mario123456@ufounders-db.d72zqjx.mongodb.net/?retryWrites=true&w=majority'),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.USER}:${process.env.PASS}@ufounders-db.d72zqjx.mongodb.net/?retryWrites=true&w=majority`
+    ),
     SeedModule,
     CommonModule
   ],
