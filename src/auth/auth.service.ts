0df0d2d2c3eea02c58a693ac46106576f4be1a50
Mdelacruzmelo <mdelacruzmelo@gmail.com>
2022-08-25 23:48:40 +0200

    TASK-4 Jwt function validator

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,13 +5,18 @@ import * as bcrypt from 'bcrypt'
 
 import { LoginUserDto, CreateUserDto } from './dto';
 import { User } from './entities/user.entity';
+import { JwtPayload } from './interfaces/jwt.payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>
+    private readonly userRepository: Repository<User>,
+
+    private readonly jwtService: JwtService
+
   ) { }
 
   async create(createUserDto: CreateUserDto) {
@@ -27,7 +32,10 @@ export class AuthService {
       await this.userRepository.save(user)
       delete user.password
 
-      return user
+      return {
+        user,
+        token: this.getJwtToken({ email: user.email })
+      }
 
     } catch (error) {
 
@@ -58,11 +66,20 @@ export class AuthService {
     delete user.id
     // TODO: Crear la token
 
-    return user
+    return {
+      user,
+      token: this.getJwtToken({ email: user.email })
+    }
 
 
   }
 
+  private getJwtToken(payload: JwtPayload) {
+
+    const token = this.jwtService.sign(payload)
+    return token;
+
+  }
 
   private handleDBErrors(error: any): never {
     if (error.code === 11000) {
